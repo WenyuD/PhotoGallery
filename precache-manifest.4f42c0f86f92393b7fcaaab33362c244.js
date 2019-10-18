4be4d863b5e449488d5efaa8c5cd4e44a10eaf94
Layla Dai <Layla_Dai@epam.com>
2019-10-18 14:25:30 +0800

    Updates

---
+++ precache-manifest.4f42c0f86f92393b7fcaaab33362c244.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a704a09e01d1146d6d8b7ac63cb066d",
+    "url": "/PhotoGallery/index.html"
+  },
+  {
+    "revision": "55251c878ee9632aef21",
+    "url": "/PhotoGallery/static/css/main.eabb8f1d.chunk.css"
+  },
+  {
+    "revision": "224c565d99f7b23d569a",
+    "url": "/PhotoGallery/static/js/2.9ac53c96.chunk.js"
+  },
+  {
+    "revision": "55251c878ee9632aef21",
+    "url": "/PhotoGallery/static/js/main.41b4a85c.chunk.js"
+  },
+  {
+    "revision": "b811706a96c8eab1f614",
+    "url": "/PhotoGallery/static/js/runtime-main.57501087.js"
+  },
+  {
+    "revision": "92498b0d22ad460a9b68117331b4e135",
+    "url": "/PhotoGallery/static/media/arrow.92498b0d.svg"
+  },
+  {
+    "revision": "131f9dace50c10f1a23c1e910a6cc272",
+    "url": "/PhotoGallery/static/media/brydes_whale.131f9dac.jpg"
+  },
+  {
+    "revision": "d2cc82f7c9fdcfc987de36670b3f1b84",
+    "url": "/PhotoGallery/static/media/grey_whale.d2cc82f7.jpg"
+  },
+  {
+    "revision": "ba6d0102dc7e4e5981f88cdb5a26991b",
+    "url": "/PhotoGallery/static/media/humpback_whale_breaching.ba6d0102.jpg"
+  },
+  {
+    "revision": "60c8a541e9892069d3f891924395404c",
+    "url": "/PhotoGallery/static/media/humpback_whale_fluking.60c8a541.jpg"
+  },
+  {
+    "revision": "eef1c2035014029689cbdbaa9942d4d8",
+    "url": "/PhotoGallery/static/media/sperm_whale_fluking.eef1c203.jpg"
+  }
+]);
